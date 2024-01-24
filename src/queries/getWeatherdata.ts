48cb7cc90dded1bff5a13002fae2ba5e33feabfb
aparcanapavel <aparcanapavel@gmail.com>
2024-01-24 17:14:48 -0500

    fix(accessibility): change weather api to main endpoint

--- src/queries/getWeatherdata.ts
+++ src/queries/getWeatherdata.ts
@@ -1,7 +1,7 @@
 export default async function getWeatherdata() {
   try {
     const res = await fetch(
-      "http://transit-api-alb-365197636.us-east-1.elb.amazonaws.com/api/get-weather-data",
+      `${process.env.TRANSIT_API_URL}/get-weather-data`,
       { next: { revalidate: 3600 } }
     );
     if (!res.ok) throw new Error("failed to fetch weather data");
