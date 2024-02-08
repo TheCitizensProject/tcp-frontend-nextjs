37678443da0b3a1012485bae1e6cb6355aeb5a23
paulsontiti@gmail.com <paulsontiti@gmail.com>
2024-02-08 09:03:53 +0100

    center icons when arrow icons are hidden
    icons are centered even when arrow icons are hidden

--- src/app/(pages)/transit/component/TransitHeader.tsx
+++ src/app/(pages)/transit/component/TransitHeader.tsx
@@ -22,53 +22,60 @@ const headerContents: HeaderContentType[] = [
   { icon: <TbAlertCircleFilled className="w-8 h-8" />, text: "Alerts" },
   { icon: <TiWeatherPartlySunny className="w-8 h-8" />, text: "Weather" },
   { icon: <MdStore className="w-8 h-8" />, text: "Store" },
+  { icon: <TiWeatherPartlySunny className="w-8 h-8" />, text: "Weathers" },
+  { icon: <MdStore className="w-8 h-8" />, text: "Stores" },
 ];
 export default function TransitHeader() {
   const [startIndex, setStartIndex] = useState(0);
   const [endIndex, setEndIndex] = useState(3);
+
   return (
     <div
       className="flex items-center justify-center
      mt-4 mb-10 gap-8 w-full"
     >
       {/* show only when the fourth item is displaying */}
-      {startIndex > 1 && (
+      {startIndex > 1 ? (
         <div className="w-1/12 grow">
           <MdArrowBackIosNew
             className="w-8 h-8"
             onClick={() => {
-              if (startIndex > 0) {
-                setStartIndex((prv) => prv - 3);
-                setEndIndex((prv) => prv - 3);
-              }
+              setStartIndex((prv) => prv - 3);
+              setEndIndex((prv) => prv - 3);
             }}
           />
         </div>
+      ) : (
+        //this ensures that the icons are centered even when the arrow button is hidden
+        <div className="w-1/12 grow"></div>
       )}
       <div className="flex justify-between grow w-10/12 overflow-hidden">
-        {headerContents.slice(startIndex, endIndex).map((content) => (
-          <div
-            key={content.text}
-            className="flex flex-col items-center justify-center"
-          >
-            {content.icon}
-            <span>{content.text}</span>
-          </div>
-        ))}
+        {headerContents.slice(startIndex, endIndex).map((content) => {
+          return (
+            <div
+              key={content.text}
+              className="flex flex-col items-center justify-center"
+            >
+              {content.icon}
+              <span>{content.text}</span>
+            </div>
+          );
+        })}
       </div>
       {/* show only when the last three items are not displaying */}
-      {endIndex < headerContents.length && (
+      {endIndex < headerContents.length ? (
         <div className="w-1/12 grow flex justify-end">
           <MdArrowForwardIos
             className="w-8 h-8"
             onClick={() => {
-              if (endIndex <= headerContents.length - 1) {
-                setStartIndex((prv) => prv + 3);
-                setEndIndex((prv) => prv + 3);
-              }
+              setStartIndex((prv) => prv + 3);
+              setEndIndex((prv) => prv + 3);
             }}
           />
         </div>
+      ) : (
+        //this ensures that the icons are centered even when the arrow button is hidden
+        <div className="w-1/12 grow"></div>
       )}
     </div>
   );
